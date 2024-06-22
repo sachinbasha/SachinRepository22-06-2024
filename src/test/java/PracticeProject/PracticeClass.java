4b7c46e14d43d5c561b4a16de06fba4e6a5ad346
user <user@DESKTOP-EULJV3S>
2024-06-22 16:40:01 +0530

    k

---
+++ src/test/java/PracticeProject/PracticeClass.java
@@ -0,0 +1,20 @@
+package PracticeProject;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PracticeClass {
+
+	@Test(groups="smoke")
+	public void test()
+	{
+		System.out.println("i am sachin 22-06-2024");
+		
+	}
+	@Test
+	public void test1()
+	{
+		System.out.println("boss iam pass");
+		
+	}
+}
